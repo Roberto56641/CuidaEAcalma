0481228e99f0b83897cb1c3d0e2217d1036df3a5
Roberto56641 <jose-roberto-pr@hotmail.com>
2025-11-23 17:57:04 -0300

    Criando arquivo app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+// Função de boas-vindas
+function startApp() {
+  alert("Bem-vindo ao Cuida & Acalmar!");
+}
+
+// Função para abrir atividades (ainda vamos melhorar)
+function openActivity(activityName) {
+  alert("Abrindo: " + activityName);
+}
